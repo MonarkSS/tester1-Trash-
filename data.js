4ac72d8a5bf858e1c93fb72775f6baa2933a9fef
MonarkSS <shirjeel2000@gmail.com>
2025-12-30 10:33:39 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360photoconverter",
+      "name": "360photoconverter",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.8250822915770861,
+        "pitch": -0.07772194384993014,
+        "fov": 1.1874993334215422
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1652568671163905,
+          "pitch": 0.23637823107283396,
+          "rotation": 18.84955592153877,
+          "target": "0-360photoconverter"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.017616907454327,
+          "pitch": -0.49461557550541535,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
