e8140860c748dfdb163cdee5f6409c55ac0cf480
MonarkSS <shirjeel2000@gmail.com>
2025-12-30 11:07:43 +0500

    DOne

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-360photoconverter",
-      "name": "360photoconverter",
+      "id": "0-t3",
+      "name": "t3",
       "levels": [
         {
           "tileSize": 256,
@@ -20,24 +20,29 @@ var APP_DATA = {
       ],
       "faceSize": 1000,
       "initialViewParameters": {
-        "yaw": -0.8250822915770861,
-        "pitch": -0.07772194384993014,
-        "fov": 1.1874993334215422
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.1652568671163905,
-          "pitch": 0.23637823107283396,
-          "rotation": 18.84955592153877,
-          "target": "0-360photoconverter"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": -2.017616907454327,
-          "pitch": -0.49461557550541535,
-          "title": "Title",
-          "text": "Text"
+          "yaw": 0.11148507765946647,
+          "pitch": -0.057166524596890866,
+          "title": "Door #31",
+          "text": "Something you desire the most"
+        },
+        {
+          "yaw": 1.7769358942008262,
+          "pitch": -0.12366197452085714,
+          "title": "Door #32",
+          "text": "Something that you don't really want but it's the best choice for you"
+        },
+        {
+          "yaw": -1.5488995952186375,
+          "pitch": -0.05181610244937218,
+          "title": "Room #30",
+          "text": "Something you secretly want but haven't even admitted to yourself"
         }
       ]
     }
